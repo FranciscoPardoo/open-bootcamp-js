110374c1d9da0cebf5632760b6c17a33d5e652d9
FranciscoPardoo <105252301+FranciscoPardoo@users.noreply.github.com>
2023-04-14 16:51:15 -0300

    segundo ejercicio

---
+++ array.js
@@ -0,0 +1,15 @@
+const array =[
+    ficha = {
+            nombre: "Francisco",
+            edad: 21,
+            "¿eres desarrollador?": true,
+            nacimiento: new Date(2001, 10, 30),
+            "libro favorito":{
+                titulo: "El principito",
+                autor: "Antoine de Saint-Exupéry", 
+                fecha: new Date( 1943, 3, 6)
+            }
+        }
+    ]
+
+    console.log(array)
